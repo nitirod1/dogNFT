31d4c01c5d1efea36ec72e0d978d3f9f1a73126c
subder <subtawee.ng@gmail.com>
2023-04-15 22:55:10 +0700

    version 1.01

--- contracts/dogNFT.sol
+++ contracts/dogNFT.sol
@@ -3,26 +3,30 @@ pragma solidity 0.8.19;
 
 import "erc721a/contracts/ERC721A.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DogNFT is ERC721A{
+contract DogNFT is ERC721A , AccessControl{
     using Strings for uint256;
     uint256 public constant MAX_SUPPLY = 20;
-    uint256 public constant START_TOKEN_ID = 1;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
 
+    uint256 internal START_TOKEN_ID ;
     string public baseURI;
     string internal baseExtension = ".json";
+    bytes32 internal constant Controller = keccak256("Controller");
 
     address public immutable owner;
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Capybara: not owner");
-        _;
-    }
 
     /**
      * @dev Constructor function
      */
-    constructor() ERC721A("Capybara NFT", "CAPY") {
+    constructor() ERC721A("PupPixel", "PPX") {
         owner = msg.sender;
+        _tokenIdCounter.increment();
+        START_TOKEN_ID = _tokenIdCounter.current();
+        _grantRole(Controller, msg.sender);
     }
 
     /**
@@ -30,16 +34,17 @@ contract DogNFT is ERC721A{
      * @return uint256 token ID
      * override start token id
      */
-    function _startTokenId() internal view virtual override returns (uint256) {
+     function _startTokenId() internal view virtual override returns (uint256) {
         return START_TOKEN_ID;
     }
 
-    function mint(uint256 quantity) public payable {
+
+    function mint() external payable onlyRole(Controller){
         require(
-            totalSupply() + quantity <= MAX_SUPPLY,
+            totalSupply()+1 <= MAX_SUPPLY,
             "Capybara: exceed max supply"
         );
-        _mint(msg.sender, quantity);
+        _mint(msg.sender, 1);
     }
 
     function tokenURI(
@@ -64,7 +69,13 @@ contract DogNFT is ERC721A{
         return baseURI;
     }
 
-    function setBaseURI(string memory _newBaseURI) external onlyOwner {
+    function setBaseURI(string memory _newBaseURI) external onlyRole(Controller) {
         baseURI = _newBaseURI;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view override(ERC721A, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const DogNFT = await ethers.getContractFactory("DogNFT");
+  const dogNFT = await DogNFT.deploy();
+  await dogNFT.deployed();
 
-  const Lock = await ethers.getContractFactory("DogNFT");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("dogNFT deployed to:", dogNFT.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
